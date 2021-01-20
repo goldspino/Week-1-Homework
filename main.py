058df64b5f79bf2088b35322a7e3a53b72b6a1b4
Sathish Ramachandran <sathish.r26@gmail.com>
2021-01-20 02:08:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+natural_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+strl1 = "abc"
+strl2 = "def"
+strl3 = strl1+strl2
+int1 = 1
+int2 = 2
+int3 = 3
+int4 = int1+int2
+int5 = int4+int3
+bool1 = True
+bool2 = True
+bool3 = False
+bool4 = True
+bool5 = False
+cars = ("Ferrari, "  "Lamborgini, "  "Jaguar")
+print(cars)
